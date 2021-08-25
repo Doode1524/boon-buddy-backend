33541d6db9c3b3311a76e2ff76e4feba3f22b59f
joey <doode1524@gmail.com>
2021-08-25 19:03:26 -0400

    ares boons up to duos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 
+########################
+######## GODS #########
+########################
+
 God.create(name: "Aphrodite", title: "Goddess of Love And Beauty", image: "https://i.redd.it/uhmoj30m8ni61.jpg", icon: "https://static.wikia.nocookie.net/hades_gamepedia_en/images/1/1d/Aphrodite_symbol.png/revision/latest/scale-to-width-down/305?cb=20181212020221")
 
 God.create(name: "Ares", title: "God of War", image: "https://i.pinimg.com/originals/e6/51/15/e651150cdb86e42473a06cbd0cb03ef2.jpg", icon: "https://static.wikia.nocookie.net/hades_gamepedia_en/images/c/c9/Ares_symbol.png/revision/latest/scale-to-width-down/283?cb=20181212020222")
@@ -16,8 +20,9 @@ God.create(name: "Zeus", title: "King of the Olympians", image: "https://lh3.goo
 God.create(name: "Chaos", title: "Primordial Originator", image: "https://lh3.googleusercontent.com/rJfP2RH9wLYz884dXCqP0GdNkEFGDyIYGhp5LTXQtriN4TeTDurUpaVngHP1QIOd0caS=s85", icon: "https://static.wikia.nocookie.net/hades_gamepedia_en/images/1/14/Chaos_symbol.png/revision/latest/scale-to-width-down/305?cb=20190119213305")
 
 
-
+########################
 ######## BOONS #########
+########################
 
 ######## Aphrodite (1) #########
 Boon.create(name: "Heartbreak Strike", description: "Your Attack deals more damage and inflicts Weak.")
@@ -66,10 +71,55 @@ Duo.create(boon_id: 13, god_id: 1) # Broken Resolve
 Duo.create(boon_id: 14, god_id: 1) # Blown Kiss
 Duo.create(boon_id: 15, god_id: 1) # Unhealthy Fixation (Legendary)
 
-# Boon.create(name: "Curse of Longing", description: "Your Doom effects continuously strike Weak foes.", duo: true)
 
+######## Ares (2) #########
 
-######## Ares #########
+Boon.create(name: "Curse of Agony", description: "Your Attack inflicts Doom.")
+
+Boon.create(name: "Curse of Pain", description: "Your Special inflicts Doom.")
+
+Boon.create(name: "Slicing Shot", description: "Your Cast sends a Blade Rift hurling ahead.")
+
+Boon.create(name: "Slicing Flare", description: "Your Cast sends a large Blade Rift hurling ahead for a brief time.")
+
+Boon.create(name: "Blade Dash", description: "Your Dash creates a Blade Rift where you started.")
+
+Boon.create(name: "Ares' Aid", description: "Your Call turns you into an Impervious Blade Rift for 1.2 Sec.")
+
+Boon.create(name: "Curse of Vengeance", description: "After you take damage, inflict Doom on surrounding foes.")
+
+Boon.create(name: "Urge to Kill", description: "Your Attack, Special, and Cast deal more damage.")
+
+Boon.create(name: "Battle Rage", description: "After slaying a foe, your next Attack or Special deals more damage.")
+
+Boon.create(name: "Blood Frenzy", description: "After using Death Defiance, deal more damage that encounter.")
+
+Boon.create(name: "Black Metal", description: "Your Blade Rift powers deal damage in a wider area.", prerequisites: ["Slicing Flare", "Slicing Shot", "Blade Dash", "Ares' Aid"])
+
+Boon.create(name: "Engulfing Vortex", description: "Your Blade Rift effects last longer and pull foes in.", prerequisites: ["Slicing Flare", "Slicing Shot", "Blade Dash", "Ares' Aid"])
+
+Boon.create(name: "Dire Misfortune", description: "Your Doom effects deal more damage when applied multiple times.", prerequisites: ["Curse of Agony", "Curse of Pain"])
+
+Boon.create(name: "Impending Doom", description: "Your Doom effects deal more damage, after +0.5 Sec.", prerequisites: ["Curse of Agony", "Curse of Pain", "Curse of Vengeance"])
+
+Boon.create(name: "Vicious Cycle", description: "Your Blade Rift effects deal more damage for each consecutive hit.", legendary: true, prerequisites: ["Black Metal", "Engulfing Vortex"])
+
+Duo.create(boon_id: 16, god_id: 2) # Curse of Agony
+Duo.create(boon_id: 17, god_id: 2) # Curse of Pain
+Duo.create(boon_id: 18, god_id: 2) # Slicing Shot
+Duo.create(boon_id: 19, god_id: 2) # Slicing Flare
+Duo.create(boon_id: 20, god_id: 2) # Blade Dash
+Duo.create(boon_id: 21, god_id: 2) # Ares' Aid
+Duo.create(boon_id: 22, god_id: 2) # Curse of vengeance
+Duo.create(boon_id: 23, god_id: 2) # Urge to Kill
+Duo.create(boon_id: 24, god_id: 2) # Battle Rage
+Duo.create(boon_id: 25, god_id: 2) # Blood Frenzy
+Duo.create(boon_id: 26, god_id: 2) # Black Metal
+Duo.create(boon_id: 27, god_id: 2) # Engulfing vortex
+Duo.create(boon_id: 28, god_id: 2) # Dire Misfortune
+Duo.create(boon_id: 29, god_id: 2) # Impending Doom
+Duo.create(boon_id: 30, god_id: 2) # Vicious Cycle
+
 
 ######## Duos #########
 
